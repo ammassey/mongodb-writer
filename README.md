5b6fbf7672c705b04e5ada7dca7e9bc2d90f9f7e
ammassey <45047765+ammassey@users.noreply.github.com>
2024-08-04 23:45:27 -0700

    implement microservice for mongo writer

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # mongodb-writer
+This is a microservice written by Alan Massey for Andronik Mkrtytchev
+to provide write functionality for users who would like enter a 
+recipe manually in the event they did not receive a desired recipe by
+the api Andronik's program integrates with.
+
+Communication Contract:
+Communication will be established with the microservice via rest API. I agree to be available in the event that Andronik is unable to integrate this
+microservice into his main program. My availablity it 5PM-Midnight PST and open 
+availability on weekends. Once hosted, I will provide the correct endpoint for Andronik
+and troubleshoot any issue.
+
+Posting Data:
+Posting data to MongoDB by why of this microservice requires the data to be formatted as follows:
+{
+    "title": "",
+    "ingredients": "",
+    "servings": "",
+    "instructions": "",
+    "Password": "",
+    "user": ""
+}
+
+There is no header required an example call would be as follows:
+requests.post(url, data)
+
+Response Data: 
+The response that the user will receive from the server is as the text as follows:
+{"_id":"","title":"","":"":"","instructions":"","password":"","user":""}
+
+A 201 status code indicates that the write to mongoDB was successful:
+
+response = requests.post(url, data)
+print(response.status_code)
+print(response.text)
+
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Recipes struct {
+	ID           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title        string             `json:"title"`
+	Ingredients  string             `json:"ingredients"`
+	Servings     string             `json:"servings"`
+	Instructions string             `json:"instructions"`
+	Password     string             `json:"password"`
+	User         string             `json:"user"`
+}
+
+var collection *mongo.Collection
+
+func main() {
+
+	godotenv.Load()
+
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer client.Disconnect(context.Background())
+
+	err = client.Ping(context.Background(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+	collection = client.Database("test").Collection("recipes")
+
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	app.Post("/api/recipes", postRecipes)
+
+	port := os.Getenv("PORT")
+	log.Fatal(app.Listen("0.0.0.0:" + port))
+}
+
+func postRecipes(c *fiber.Ctx) error {
+
+	recipe := new(Recipes)
+
+	if err := c.BodyParser(recipe); err != nil {
+		return err
+	}
+
+	insertResult, err := collection.InsertOne(context.Background(), recipe)
+	if err != nil {
+		return err
+	}
+
+	recipe.ID = insertResult.InsertedID.(primitive.ObjectID)
+
+	return c.Status(201).JSON(recipe)
+}
